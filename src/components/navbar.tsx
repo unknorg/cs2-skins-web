e093a69279e2cc73bdf888c24fa07dd678de5491
Hathoute <whitesmith.thedj@gmail.com>
2023-11-14 23:45:23 +0100

    ci: setup Dockerfile and github workflows

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -15,10 +15,10 @@ export default function Navbar({session}: NavbarProps) {
   if (!session?.user) {
     return (<div>Unauthenticated</div>)
   }
-  
+
   return (
       <>
-        <LinkModal seqId={modalSeqId} token="aabbccdd"/>
+        <LinkModal seqId={modalSeqId}/>
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="https://flowbite.com/" className="flex items-center">
@@ -33,13 +33,13 @@ export default function Navbar({session}: NavbarProps) {
             </a>
             <div className="flex items-center md:order-2">
               <span>Welcome <b>{session.user.name}</b></span>
-              <a href="/api/auth/signout">
+              <Link href="/api/auth/signout">
                 <button type="button"
                         className="text-white ml-3 my-auto bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   Logout
                 </button>
-              </a>
+              </Link>
             </div>
             <div
                 className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
